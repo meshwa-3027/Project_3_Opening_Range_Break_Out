bc5ce01ce89fe459728faa9db9f788698b5cce6a
Meshwa Patel <125075708+meshwa-3027@users.noreply.github.com>
2024-06-30 15:35:19 +0530

    Add files via upload

---
+++ alice_credentials.py
@@ -0,0 +1,21 @@
+from pya3 import *
+
+user_id = "AB187400"
+api_key = "cjVq0mmo9XD6sWlGWZfIXVED1qJnd5KA3Ms7Ba4zx72O8bHpItVMusnF3rhak094jnAfmm2LbPftfKY97xP5Ldy6HQoa5igcKXYR85KOV3pUpZCzx0TPrtjhXC2n3xYu"
+
+def login():
+
+	alice = Aliceblue(user_id= user_id,api_key= api_key)
+	alice.get_session_id() # Get Session ID
+
+	print("Auto Login Success")
+
+
+	# alice.get_contract_master("MCX")
+	# alice.get_contract_master("NFO")
+	alice.get_contract_master("NSE")
+	# alice.get_contract_master("BSE")
+	# alice.get_contract_master("CDS")
+	# alice.get_contract_master("BFO")
+	# alice.get_contract_master("INDICES")
+	return alice
